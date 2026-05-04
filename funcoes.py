4e8137929b6850c9dabc2e45a90438a34bef4f97
bernardonp43 <bernardonp@al.insper.edu.br>
2026-05-04 16:47:53 -0300

    remover dado

--- funcoes.py
+++ funcoes.py
@@ -5,16 +5,15 @@ def rolar_dados (n):
         dados.append(random.randint(1, 6))
     return dados
 
-def guardar_dado(d, dg, p):
-    novo = [] 
-    for x in dg:
-        novo.append(x)    
-    novo.append(d[p])    
-    return [dg, novo]
-
-def remover_dado(dg, p):
-    x = {}
-    for c in dg:
-        if c != p:
-            x[c] = dg[c]
-    return x
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    novo_estoque = []
+    for x in dados_no_estoque:
+        novo_estoque.append(x)
+    novo_estoque.append(dados_rolados[dado_para_guardar])
+    
+    novos_rolados = []
+    for i in range(len(dados_rolados)):
+        if i != dado_para_guardar:
+            novos_rolados.append(dados_rolados[i])
+    
+    return [novos_rolados, novo_estoque]
