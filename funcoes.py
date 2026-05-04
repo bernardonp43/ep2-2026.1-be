2576eca16da5998964fe9be8d7c763e5dd4978e2
bernardonp43 <bernardonp@al.insper.edu.br>
2026-05-04 13:43:23 -0300

    adiciona rolar_dados

--- funcoes.py
+++ funcoes.py
@@ -1,9 +1,6 @@
 import random
-def rolar_dados (d):
+def rolar_dados (n):
     dados = []
-    for i in range(5):
-        if i in d:
-            dados.append(d[i])
-        else:
-            dados.append(random.randint(1, 6))
+    for i in range(n):
+        dados.append(random.randint(1, 6))
     return dados
