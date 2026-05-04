06b6d96d5b18ef83df843cd57524cb9a135b4172
bernardonp43 <bernardonp@al.insper.edu.br>
2026-05-04 17:09:01 -0300

    cp soma

--- funcoes.py
+++ funcoes.py
@@ -40,3 +40,9 @@ def calcula_pontos_regra_simples(dados):
                 pontos += face
         resultado[face] = pontos
     return resultado
+
+def calcula_pontos_soma(dados):
+    total = 0
+    for dado in dados:
+        total += dado
+    return total
